587e7fa166ec2f25e332f231e85c612275feaa12
Mila Makarenko <=>
2024-02-07 13:19:41 +0200

    Text.txt

--- src/movieController.js
+++ src/movieController.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const fs = require ('fs');
 
 router.get('/', (req, res) => {
-     fs.appendFileSync('test.txt', 'hello');
-     res.send('movies');
-
+     //fs.appendFileSync('test.txt', 'hello');
+     let text = fs.readFileSync('text.txt', 'utf8');
+     console.log(text);
+     res.send(''+text);
 });
 
 module.exports = router;
