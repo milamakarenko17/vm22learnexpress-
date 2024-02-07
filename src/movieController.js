43f25d71c1cd5dcfa4f1c9b3ed00606b9fec102c
Mila Makarenko <=>
2024-02-07 13:11:33 +0200

    Add fs.appendFileSync

--- src/movieController.js
+++ src/movieController.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const fs = require ('fs');
 
 router.get('/', (req, res) => {
+     fs.appendFileSync('test.txt', 'hello');
      res.send('movies');
 
 });
