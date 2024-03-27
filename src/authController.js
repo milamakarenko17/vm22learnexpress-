9a340bddc97b5f18ebb347d5ea9c6b596e1a988f
Mila Makarenko <=>
2024-03-27 15:01:41 +0200

    Add Register

--- src/authController.js
+++ src/authController.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('/models/User.js');
+
 router.get('/register', async (req, res) => {
     res.render('/auth/register.njk');
+  });
+
+  router.get('/register', async (req, res) => {
+    let user = await User.findOne({
+       where: {
+        email: req.body.email
+       }
+    });
+    if(req.body.password !== req.body.password_confirm || user){
+    } else {
+        User.create ({
+            name: req.body.name,
+            email: req.body.email,
+            pasword: req.body.password,
+        })
+    }
+    res.redirect('/register');
   });
 
   module.exports = router;
