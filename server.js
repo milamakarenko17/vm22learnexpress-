27eba0a72ff19a5e088eb27350b1d7e88860bd8a
Mila Makarenko <=>
2024-04-03 14:00:42 +0300

    Add Login

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+console.log('hello terminal');
+
 const express = require('express');
-const nunjucks = require('nunjucks');
+const nunjucks = require('nunjucks')
 const app = express();
 const port = 3000;
 const cookieParser = require('cookie-parser');
@@ -7,58 +9,62 @@ app.use(cookieParser());
 
 const session = require('express-session');
 app.use(session({
-   secret: 'secret'
+  secret:'secret'
 }));
 
-
-app.use(express.urlencoded({
-  extended:true
-}));
+app.use(express.urlencoded({extended:true}))
 
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
 
+
+app.use ((req, res) => {
+  env.addGlobal('user', req.session.user);
+  next();
+}); 
+
 app.get('/', (req, res) => {
-  res.render('index.njk');
-});
+    console.log(req.session.user);
+    res.render('index.njk');
+
+  });
 
 app.get('/page2', (req, res) => {
-    res.render('page2.njk');
-  });
-
-  app.get('/form', (req, res) => {
-    console.log (req.query)
-    res.render('form.njk', req.query);
-  });
-
-  app.get('/circle', (req, res) => {
-    console.log(req.query);
-    res.render('circle.njk');
-  });
-
-  app.post('/circle', (req, res) => {
-    let area = Math.PI * req.body.radius * req.body.radius;
-    let circumference = Math.PI * req.body.radius * 2;
-    let volume = Math.PI * req.body.radius * req.body.radius * req.body.radius * 4/3;
-    res.render('circleAnswer.njk', {r: req.body.radius, a: area, c: circumference, v: volume})
+  res.render('page2.njk');
 });
 
-const movieController = require('./src/movieController.js');
-app.use('/movies', movieController);
+app.get('/form', (req, res) => {
+  console.log(req.query);
+  res.render('form.njk', req.query);
+});
 
-app.get('/cookie', (req, res) => {
- res.cookie('mycookie' , 'cool cookie' , {maxAge:1000*60*60*24*31});
- if(!req.session.secretValue){
- req.session.secretValue = 'shhh';
-}
- res.send(req.session);
+app.get('/circle', (req, res) => {
+  console.log(req.query);
+  res.render('circle.njk');
+});
+
+app.post('/circle', (req, res) => {
+  let area= Math.PI * req.body.radius * req.body.radius;
+  let circumference= 2*Math.PI * req.body.radius;
+  let volume= 4/3*Math.PI  * req.body.radius * req.body.radius * req.body.radius;
+  res.render('circleAnswer.njk', {r: req.body.radius, a:area, c:circumference, v:volume});
+});
+
+const movieController = require('./src/movieController.js')
+app.use('/movies', movieController)
+
+app.get('/cookie', (req,res) => {
+  res.cookie('mycookie', 'coke cookie', {maxAge: 1000*60*60*24*30});
+  if(!req.session.secretValue){ 
+  req.session.secretValue = 'Victoria Secret';}
+  res.send(req.session);
+
 });
 
 const authController = require('./src/authController.js');
 app.use(authController);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
--- src/authController.js
+++ src/authController.js
@@ -1,27 +1,50 @@
 const express = require('express');
 const router = express.Router();
-
-const User = require('/models/User.js');
+const bcrypt = require('bcrypt');
+const User = require('./models/User.js');
 
 router.get('/register', async (req, res) => {
-    res.render('/auth/register.njk');
-  });
+    res.render('auth/register.njk');
+});
 
-  router.get('/register', async (req, res) => {
+router.post('/register', async (req, res) => {
     let user = await User.findOne({
-       where: {
-        email: req.body.email
-       }
+        where: {
+            email: req.body.email
+        }
     });
-    if(req.body.password !== req.body.password_confirm || user){
+    if (req.body.password !== req.body.password_confirm || user) {
+        res.redirect('/register');
     } else {
-        User.create ({
+        User.create({
             name: req.body.name,
             email: req.body.email,
-            pasword: req.body.password,
-        })
-    }
-    res.redirect('/register');
-  });
+            password: bcrypt.hashSync(req.body.password, 12)
+        });
+        res.redirect('/');
 
-  module.exports = router;
+    }
+});
+
+router.get('/login', async (req, res) => {
+  res.render('auth/login.njk');
+});
+
+router.post('/login', async (req, res) => {
+  let user = await User.findOne({
+    where: {
+        email: req.body.email
+    }
+ });
+ console.log(user, bcrypt.compareSync(req.body.password, user.password ))
+    if (!user || !bcrypt.compareSync(req.body.password, user.password )){
+    res.redirect('/login');
+  } 
+  else {
+    req.session.user = user;
+    res.redirect('/');
+  }
+});
+
+
+module.exports = router;
